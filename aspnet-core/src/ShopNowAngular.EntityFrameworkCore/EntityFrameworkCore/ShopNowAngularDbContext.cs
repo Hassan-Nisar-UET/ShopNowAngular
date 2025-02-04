8dbb685bcaa4af7373d73723ce9862b34f187771
Hassan Nisar <hassannisar808@gmail.com>
2025-02-05 00:27:45 +0500

    Your commit message

---
+++ aspnet-core/src/ShopNowAngular.EntityFrameworkCore/EntityFrameworkCore/ShopNowAngularDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Abp.Zero.EntityFrameworkCore;
+using ShopNowAngular.Authorization.Roles;
+using ShopNowAngular.Authorization.Users;
+using ShopNowAngular.MultiTenancy;
+
+namespace ShopNowAngular.EntityFrameworkCore
+{
+    public class ShopNowAngularDbContext : AbpZeroDbContext<Tenant, Role, User, ShopNowAngularDbContext>
+    {
+        /* Define a DbSet for each entity of the application */
+        
+        public ShopNowAngularDbContext(DbContextOptions<ShopNowAngularDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
